f6741bc1cae2560353df8981f0d91a87e50fed70
Suvam Behera <suvambehera21@gmail.com>
2024-04-19 17:40:30 +0530

    updated cors and config

--- src/main/java/com/demo/shutterstockApi/controller/Controller.java
+++ src/main/java/com/demo/shutterstockApi/controller/Controller.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(
@@ -32,5 +29,12 @@ public class Controller {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto("200", "Data posted successfully"));
+    }
+
+    @GetMapping("/get-data")
+    public ResponseEntity<ResponseDto> getData() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto("200", "Data retrieved successfully"));
     }
 }
